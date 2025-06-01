4ede5e69fc9702883cac45bb34c0235d832f94cb
Bhoomi Pansuriya <bhoomi.pansuriya@gmail.com>
2025-06-01 13:20:12 +0530

    Session 9 properties and indexers

---
+++ OOP/Session9_PropetiesAndIndexer/ATMPin.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session9_PropetiesAndIndexer
+{
+    internal class ATMPin
+    {
+        private string _pin;
+        public string Pin
+        {
+            get { return _pin; }
+            set
+            {
+
+                if (value != null && value != "")
+                {
+                    if (value.Length == 4 || value.Length == 6)
+                    {
+                        bool isValid = true;   
+                        for (int i = 0; i < value.Length; i++)
+                        {
+                            if (value[i] < 48 || value[i] > 57)
+                            {
+                                isValid = false;
+                            }
+                        }
+                        if (isValid)
+                        {
+                            _pin = value;
+                        } else
+                        {
+                            System.Console.WriteLine("PIN contains one or more non-digit characters.");
+                        }
+                    }
+                    else System.Console.WriteLine("PIN should contain either 4 digits or 6 digits");
+
+
+                } else 
+                System.Console.WriteLine("PIN value can't be blank");
+
+            }
+        }
+    }
+}
---
+++ OOP/Session9_PropetiesAndIndexer/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Session9_PropetiesAndIndexer;
+
+Console.WriteLine("Hello, World!");
+ATMPin aTMPin = new ATMPin();
+aTMPin.Pin = "1232";
+Console.WriteLine(aTMPin.Pin);
+Console.ReadKey();
